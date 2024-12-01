cd7ba1c415cbb5f0d448aa40d8dae58bde1c9b5f
Urval Kheni <kheniurval777@gmai.com>
2024-12-01 22:20:42 +0530

    Final Commit

--- Pro/numberswappointer.c
+++ Pro/numberswappointer.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-void swap(int *a,int *b){
-    *a = *a + *b;
-    *b = *a - *b;
-    *a = *a - *b;
+void swap(int *x,int *y){
+    *x = *x + *y;
+    *y = *x - *y;
+    *x = *x - *y;
 }
 int main(){
     int x,y;
