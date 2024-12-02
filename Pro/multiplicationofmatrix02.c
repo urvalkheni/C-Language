e3472f78469be6ff6a11dd2cb07093bbb31ffae4
Urval Kheni <kheniurval777@gmai.com>
2024-12-02 19:30:48 +0530

    Final Commit

---
+++ Pro/multiplicationofmatrix02.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main(){
+    int m,n,p,q,cr;
+    printf("Enter The Row of first Matrix :: ");
+    scanf("%d",&m);
+    printf("Enter The Column of first Matrix :: ");
+    scanf("%d",&n);
+    printf("Enter The Row of Second Matrix :: ");
+    scanf("%d",&p);
+    printf("Enter The Column of Second Matrix :: ");
+    scanf("%d",&q);
+    
+    int a[m][n];
+    int b[p][q];
+    int res[m][q] = {};
+    printf("First Matrix ::\n");
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            printf("a[%d][%d] = ",i+1,j+1);
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Second Matrix ::\n");
+    for(int i=0;i<p;i++){
+        for(int j=0;j<q;j++){
+            printf("b[%d][%d] = ",i+1,j+1);
+            scanf("%d",&b[i][j]);
+        }
+    }
+   if(n==p){ 
+    cr = n;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<q;j++){
+            for(int k=0;k<cr;k++){
+                res[i][j] += a[i][k]*b[k][j];
+            }
+        }
+    }}
+    else printf("These matrix Can't be multiplyed...\n");
+    if(n==p){ for(int i=0;i<m;i++){
+        for(int j=0;j<q;j++){
+           printf("%d ",res[i][j]);
+        }
+        printf("\n");
+    }}
+
+    return 0;
+}
