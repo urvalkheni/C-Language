1a671864aa8f8ee44b186d6d0d2be5553d5d60dd
Urval Kheni <kheniurval777@gmai.com>
2024-12-05 13:16:35 +0530

    Final Commit

--- Pro/matrixspiral.c
+++ Pro/matrixspiral.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
 int main(){
-    
+    int n,i,j,a;
+    printf("Enter The Number :: ");
+    scanf("%d",&n);
+    int m[n][n];
+    for(i=0,a=1;i<n;i++){
+        for(j=0;j<n;j++){
+            m[i][j] = a;
+            a++;
+        }
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            printf("%d ",m[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
