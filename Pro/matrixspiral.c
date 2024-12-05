e0019bf75a351ac1a75352a91632b3372563b64a
Urval Kheni <kheniurval777@gmai.com>
2024-12-05 21:05:59 +0530

    Final Commit

--- Pro/matrixspiral.c
+++ Pro/matrixspiral.c
@@ -1,20 +1,19 @@
 #include<stdio.h>
 int main(){
-    int n,i,j,a;
-    printf("Enter The Number :: ");
+    int n,m,i,j,a;
+    printf("Enter The Row Number :: ");
     scanf("%d",&n);
+    printf("Enter The Coloumn Number :: ");
+    scanf("%d",&m);
     int m[n][n];
     for(i=0,a=1;i<n;i++){
-        for(j=0;j<n;j++){
+        for(j=0;j<m;j++){
             m[i][j] = a;
             a++;
         }
     }
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            printf("%d ",m[i][j]);
-        }
-        printf("\n");
-    }
+    // spiral Print
+    printf("\n");
+    int minr,maxc,maxr,minc;
     return 0;
 }
