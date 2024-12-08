75c8161556765f76029d056a0e2f3800fb308ae6
Urval Kheni <kheniurval777@gmai.com>
2024-12-08 09:02:11 +0530

    Corner Done

--- Program/lowerleft.c
+++ Program/lowerleft.c
@@ -5,7 +5,7 @@ int main(){
     scanf("%d",&n);
     for(i=1;i<=n;i++){
         for(j=1;j<=n;j++){
-            if(j>=i)printf("* ");
+            if(i>=j)printf("* ");
             else printf("  ");
         }
         printf("\n");
