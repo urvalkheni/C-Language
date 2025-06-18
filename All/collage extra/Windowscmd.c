3ad70bf75208b1d22977ecc2bced0a17535b9119
Urval Kheni <kheniurval777@gmail.com>
2025-06-18 14:45:00 +0530

    Windows and Linux Cmd added

---
+++ All/collage extra/Windowscmd.c
@@ -0,0 +1,30 @@
+// ✅ WINDOWS COMMANDS: IN C
+#include <stdio.h>
+
+int main() {
+    printf("================ Windows CMD Internal Commands =================\n\n");
+    printf("1. cls        > Clears the screen.\n");
+    printf("2. date       > Shows or sets the system date.\n");
+    printf("3. time       > Shows or sets the system time.\n");
+    printf("4. ver        > Displays the version of Windows.\n");
+    printf("5. vol        > Displays disk volume label and serial number.\n");
+    printf("6. dir        > Lists directory contents.\n");
+    printf("7. mkdir      > Creates a new directory.\n");
+    printf("8. rmdir      > Deletes an empty directory.\n");
+    printf("9. copy con   > Creates a new file from console.\n");
+    printf("10. type      > Displays contents of a file.\n");
+    printf("11. del       > Deletes a file.\n");
+    printf("12. rename    > Renames a file or directory.\n");
+    printf("13. move      > Moves file from one folder to another.\n");
+    printf("14. copy      > Copies files to another location.\n");
+    printf("15. chdir     > Changes the current directory.\n");
+    printf("16. doskey    > Enables command history and macros.\n");
+    printf("17. path      > Displays or sets a command search path.\n");
+    printf("18. attrib    > Displays/changes file attributes.\n");
+    printf("19. tree      > Graphical view of directory structure.\n");
+    printf("20. exit      > Quits CMD shell.\n");
+    printf("21. help      > Displays help information for commands.\n");
+    printf("22. echo      > Prints messages to the console.\n");
+    printf("23. pause     > Pauses execution and waits for user input.\n");
+    return 0;
+}
